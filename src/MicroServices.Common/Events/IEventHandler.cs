9e92ef25a6b32b009c5f8e512d7beae7cce9ec04
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-28 02:17:30 +0300

    Add Event Handler

---
+++ src/MicroServices.Common/Events/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MicroServices.Common.Events
+{
+    public interface IEventHandler<in T> where T:IEvent
+    {
+        Task HandleAsync(T @event);
+    }
+}
