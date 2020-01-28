ab4056fb92e8960608feddfa7cc6bd952a68f242
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-28 17:53:52 +0300

    Add Service to Command handler

--- src/MicroServices.Services.Activities/Handlers/CreateActivityHandler.cs
+++ src/MicroServices.Services.Activities/Handlers/CreateActivityHandler.cs
@@ -2,24 +2,42 @@
 using System.Threading.Tasks;
 using MicroServices.Common.Commands;
 using MicroServices.Common.Events;
+using MicroServices.Common.Exceptions;
+using MicroServices.Services.Activities.Services;
 using RawRabbit;
 
 namespace MicroServices.Services.Activities.Handlers
 {
-    public class CreateActivityHandler:ICommandHandler<CreateActivity>
+    public class CreateActivityHandler : ICommandHandler<CreateActivity>
     {
         private IBusClient _busClient;
+        private IActivityService _activityService;
 
-        public CreateActivityHandler(IBusClient busClient)
+        public CreateActivityHandler(IBusClient busClient, IActivityService activityService)
         {
             _busClient = busClient;
+            _activityService = activityService;
         }
 
         public async Task HandleAsync(CreateActivity command)
         {
-           Console.WriteLine($"Creating Activity : {command.Name}");
-           ActivityCreated created=new ActivityCreated(command.Id,command.UserId,command.Category,command.Name,command.Description,command.CreatedAt);
-           await _busClient.PublishAsync(created);
+            Console.WriteLine($"Creating Activity : {command.Name}");
+            try
+            {
+                await _activityService.AddAsync(command.Id, command.UserId, command.Category, command.Name,
+                    command.Description, command.CreatedAt);
+                ActivityCreated created = new ActivityCreated(command.Id, command.UserId, command.Category, command.Name, command.Description, command.CreatedAt);
+                await _busClient.PublishAsync(created);
+            }
+            catch (MicroServicesException e)
+            {
+                await _busClient.PublishAsync(new CreateActivityRejected(command.Id.ToString(), e.Message, e.Code));
+            }
+            catch (Exception e)
+            {
+                await _busClient.PublishAsync(new CreateActivityRejected(command.Id.ToString(), e.Message, "error"));
+            }
+
         }
     }
 }
